476d1a38e98d81ba6ef9c71f29e0a3a219a2079d
normandy14 <sturdyprogrammer@gmail.com>
2023-12-06 21:18:56 -0500

    Create class with isAllEven, isAllOdd, and isNoRelation methods

---
+++ app.js
@@ -0,0 +1,54 @@
+const DEBUG = false;
+
+class isNumRelation {
+	constructor(numList) {
+		this.numList = numList
+	}
+	
+	isEmpty() {
+		if (this.numList.length > 0) {
+			if (DEBUG) {console.log(this.numList.length)}
+			return false;
+		}
+		return true;
+	}
+	
+	isAllEven() {
+		var num = 0
+		for (var i =0; i < this.numList.length; i++) {
+			num = this.numList[i]
+			// console.log(num);
+			if (num % 2 != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	isAllOdd() {
+		var num = 0
+		for (var i =0; i < this.numList.length; i++) {
+			num = this.numList[i]
+			// console.log(num);
+			if (num % 2 != 1) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	isNoRelation() {
+		if (this.isAllEven() == false && this.isAllOdd() == false) {
+			return true;
+		}
+		return false;
+	}
+}
+
+var numList1 = [1, 2, 3, 4];
+// var numList1 = [1, 3, 11, 13];
+const numRelation = new isNumRelation(numList1);
+if (DEBUG) {console.log(numRelation.isEmpty())};
+console.log(numRelation.isAllEven());
+console.log(numRelation.isAllOdd());
+console.log(numRelation.isNoRelation());
