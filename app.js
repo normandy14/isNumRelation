cb60376a4462d12cf429e3e4175f8125edaaddf1
normandy14 <sturdyprogrammer@gmail.com>
2023-12-08 12:33:01 -0500

    Add comments for each test case

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ class isNumRelation {
 	
 }
 
-// Case 1
+// Case 1: Sequential numbers
 var numList = [1, 2, 3, 4];
 console.log(numList)
 var numRelation = new isNumRelation(numList);
@@ -77,7 +77,7 @@ console.log(numRelation.isAllOdd());
 console.log(numRelation.isNoRelation());
 delete numRelation;
 
-
+// Case 2: Even numbers
 var numList = [2, 4, 8, 10];
 console.log(numList)
 var numRelation = new isNumRelation(numList);
@@ -86,6 +86,7 @@ console.log(numRelation.isAllOdd());
 console.log(numRelation.isNoRelation());
 delete numRelation;
 
+//Case 3: Odd numbers
 var numList = [1, 3, 7, 11];
 console.log(numList);
 var numRelation = new isNumRelation(numList);
@@ -94,18 +95,21 @@ console.log(numRelation.isAllOdd());
 console.log(numRelation.isNoRelation());
 delete numRelation;
 
-
+// Case 4: Too many numbers
 try {
   var numList = [1, 3, 11, 13, 15, 16, 17, 18, 19, 20 , 21, 22, 23];
   const numRelation = new isNumRelation(numList);
-} catch (e) {
+} 
+catch (e) {
   console.error(e);
 }
 
+// Case 5: No numbers
 try {
   var numList = [];
   const numRelation = new isNumRelation(numList);
-} catch (e) {
+} 
+catch (e) {
   console.error(e);
 }
 
