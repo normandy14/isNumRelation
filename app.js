e50c79ac3d0beab04ee47f923d2e310fda7de7ef
normandy14 <sturdyprogrammer@gmail.com>
2023-12-08 15:06:07 -0500

    Wrap test cases in DEBUG conditional

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ class isNumRelation {
 		}
 		// check if list size is empty
 		if (this.#isEmpty(numList) == true) {
-			throw new Error("No numbers in list");	
+			throw new Error("No numbers in list");
 		}
 		this.numList = numList
 	}
-	
+
 	#isEmpty = (numList) => {
 		if (numList.length > 0) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	isAllEven = () => {
 		var num = 0
 		for (var i in this.numList) {
@@ -30,7 +30,7 @@ class isNumRelation {
 		}
 		return true;
 	}
-	
+
 	isAllOdd = () => {
 		var num = 0
 		for (var i in this.numList) {
@@ -41,75 +41,76 @@ class isNumRelation {
 		}
 		return true;
 	}
-	
+
 	isAllPrime = () => {
 		return;
 	}
-	
+
 	isAllComposite = () => {
 		return;
 	}
-	
+
 	isNoRelation = () => {
 		if (this.isAllEven() == false && this.isAllOdd() == false) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	/*
 	isAllMultipleOf() {
 		return;
 	}
 	*/
-	
+
 	// is all Positive, is all Negative
 	// is all greater than, is all less than
-	
+
 }
 
-// Case 1: Sequential numbers
-var numList = [1, 2, 3, 4];
-console.log(numList)
-var numRelation = new isNumRelation(numList);
-console.log(numRelation.isAllEven());
-console.log(numRelation.isAllOdd());
-console.log(numRelation.isNoRelation());
-delete numRelation;
+if (DEBUG) {
+	// Case 1: Sequential numbers
+	var numList = [1, 2, 3, 4];
+	console.log(numList)
+	var numRelation = new isNumRelation(numList);
+	console.log(numRelation.isAllEven());
+	console.log(numRelation.isAllOdd());
+	console.log(numRelation.isNoRelation());
+	delete numRelation;
 
-// Case 2: Even numbers
-var numList = [2, 4, 8, 10];
-console.log(numList)
-var numRelation = new isNumRelation(numList);
-console.log(numRelation.isAllEven());
-console.log(numRelation.isAllOdd());
-console.log(numRelation.isNoRelation());
-delete numRelation;
+	// Case 2: Even numbers
+	var numList = [2, 4, 8, 10];
+	console.log(numList)
+	var numRelation = new isNumRelation(numList);
+	console.log(numRelation.isAllEven());
+	console.log(numRelation.isAllOdd());
+	console.log(numRelation.isNoRelation());
+	delete numRelation;
 
-//Case 3: Odd numbers
-var numList = [1, 3, 7, 11];
-console.log(numList);
-var numRelation = new isNumRelation(numList);
-console.log(numRelation.isAllEven());
-console.log(numRelation.isAllOdd());
-console.log(numRelation.isNoRelation());
-delete numRelation;
+	//Case 3: Odd numbers
+	var numList = [1, 3, 7, 11];
+	console.log(numList);
+	var numRelation = new isNumRelation(numList);
+	console.log(numRelation.isAllEven());
+	console.log(numRelation.isAllOdd());
+	console.log(numRelation.isNoRelation());
+	delete numRelation;
 
-// Case 4: Too many numbers
-try {
-  var numList = [1, 3, 11, 13, 15, 16, 17, 18, 19, 20 , 21, 22, 23];
-  const numRelation = new isNumRelation(numList);
-} 
-catch (e) {
-  console.error(e);
+	// Case 4: Too many numbers
+	try {
+	  var numList = [1, 3, 11, 13, 15, 16, 17, 18, 19, 20 , 21, 22, 23];
+	  const numRelation = new isNumRelation(numList);
+	}
+	catch (e) {
+	  console.error(e);
+	}
+
+	// Case 5: No numbers
+	try {
+	  var numList = [];
+	  const numRelation = new isNumRelation(numList);
+	}
+	catch (e) {
+	  console.error(e);
+	}
 }
-
-// Case 5: No numbers
-try {
-  var numList = [];
-  const numRelation = new isNumRelation(numList);
-} 
-catch (e) {
-  console.error(e);
-}
-
